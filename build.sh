9d7c0b76669c432989609ff1f3e90cd755f39663
samic++ <samichentouf+git2@gmail.com>
2025-07-10 08:21:09 +0000

    add binary files for linux aarch and x86_64

--- build.sh
+++ build.sh
@@ -3,11 +3,12 @@
 cargo build --target x86_64-unknown-linux-gnu --release
 cp ./target/x86_64-unknown-linux-gnu/release/static_serve ./bin/x86_64-unknown-linux-gnu
 
+export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc
 cargo build --target aarch64-unknown-linux-gnu --release
 cp ./target/aarch64-unknown-linux-gnu/release/static_serve ./bin/aarch64-unknown-linux-gnu
 
-cargo build --target x86_64-pc-windows-gnu --release
-cp ./target/x86_64-pc-windows-gnu/release/static_serve.exe ./bin/x86_64-pc-windows-gnu.exe
+# cargo build --target x86_64-pc-windows-gnu --release
+# cp ./target/x86_64-pc-windows-gnu/release/static_serve.exe ./bin/x86_64-pc-windows-gnu.exe
 
-cargo build --target x86_64-pc-windows-msvc --release
-cp ./target/x86_64-pc-windows-msvc/release/static_serve.exe ./bin/x86_64-pc-windows-msvc.exe
+# cargo build --target x86_64-pc-windows-msvc --release
+# cp ./target/x86_64-pc-windows-msvc/release/static_serve.exe ./bin/x86_64-pc-windows-msvc.exe
