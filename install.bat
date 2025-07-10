f2ca2ebbef123f6de7ff9565e779dc08d0adf53d
samic++ <samichentouf+git2@gmail.com>
2025-07-10 10:13:21 +0200

    add toolchain install scripts

---
+++ install.bat
@@ -0,0 +1,2 @@
+@REM @curl -o rustup-init.exe https://win.rustup.rs; .\rustup-init.exe
+rustup target add x86_64-unknown-linux-gnu aarch64-unknown-linux-gnu x86_64-pc-windows-gnu x86_64-pc-windows-msvc
---
+++ install.sh
@@ -0,0 +1,2 @@
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+rustup target add x86_64-unknown-linux-gnu aarch64-unknown-linux-gnu x86_64-pc-windows-gnu x86_64-pc-windows-msvc
