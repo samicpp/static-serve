4add563bd23b0208dc749c2d63e04a9cc28de09f
samic++ <samichentouf+git2@gmail.com>
2025-08-25 14:17:58 +0000

    update precompiled binaries

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc
 cargo build --target aarch64-unknown-linux-gnu --release
 cp ./target/aarch64-unknown-linux-gnu/release/static_serve ./bin/aarch64-unknown-linux-gnu
 
-# cargo build --target x86_64-pc-windows-gnu --release
-# cp ./target/x86_64-pc-windows-gnu/release/static_serve.exe ./bin/x86_64-pc-windows-gnu.exe
+cargo build --target x86_64-pc-windows-gnu --release
+cp ./target/x86_64-pc-windows-gnu/release/static_serve.exe ./bin/x86_64-pc-windows-gnu.exe
 
 # cargo build --target x86_64-pc-windows-msvc --release
 # cp ./target/x86_64-pc-windows-msvc/release/static_serve.exe ./bin/x86_64-pc-windows-msvc.exe
